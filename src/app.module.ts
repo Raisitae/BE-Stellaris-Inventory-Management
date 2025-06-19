e3fa29ea029a66399513ae85a36034a3d567ef1a
Raisa Erlich <raisaerlich@gmail.com>
2025-06-19 07:49:20 -0300

    feat: created docker compose, connected mongodb on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
 import { SeedModule } from './seed/seed.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule, SalesModule, SeedModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProductsModule,
+    SalesModule,
+    SeedModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+  ],
   controllers: [],
   providers: [],
   exports: [],
