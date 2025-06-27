4132ae4db7294f06b24712497b8a98219af959f6
Raisa Erlich <raisaerlich@gmail.com>
2025-06-27 01:08:18 -0300

    feat: modify cors localhost to env variable

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:5173'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
